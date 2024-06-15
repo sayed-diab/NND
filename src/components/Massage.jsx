7b3e732459fbf404de4eba30759a6011c2a6ea09
Sayed diab <sayeddebo95@gmail.com>
2024-06-15 04:56:37 +0300

    last update

--- src/components/Massage.jsx
+++ src/components/Massage.jsx
@@ -1,11 +1,13 @@
 
-import one from '/public/Asset 1.svg';
-import two from '/public/Asset 2.svg';
-import four from '/public/Asset 4.svg';
-import six from '/public/Asset 6.svg';
-import eight from '/public/Asset 8.svg';
-import syringe1 from '/public/Syringe 2.svg';
-import syringe2 from '/public/Syringe 4.svg';
+import bottle1 from '/public/products/bottle-1.png';
+import bottle2 from '/public/products/bottle-2.png';
+import bottle4 from '/public/products/bottle-4.png';
+import bottle6 from '/public/products/bottle-6.png';
+import bottle6x2 from '/public/products/bottle-6+2.png';
+import bottle8 from '/public/products/bottle-8.png';
+import bottle12x2 from '/public/products/bottle-12+2.png';
+import bottle12x4 from '/public/products/bottle-12+4.png';
+import bottle24x4 from '/public/products/bottle-24+4.png';
 
 
 // eslint-disable-next-line react/prop-types
@@ -19,76 +21,24 @@ export default function Massage({ count, age }) {
                     <h4 className='mb-5 mt-10 clr'>لم نتلقي اي عـروض </h4>
                 </div>
             </label>}
-            {count == 1 && <label className='select__card mini__card one in'>
-                <div className='mb-15 txt-al'>
-                    <h3 className='mb-5 mt-10'>عبـوة </h3>
-                    <h5 className='clr'>واحـدة فقـط</h5>
-                    <h5 className='price'><span className='pr'>بــ</span><span className='gr'> 400 </span> ريـال سـعودي</h5>
-                </div>
-                <img id='fx-1' src={one} />
-            </label>
+            {count == 1 && <img className='bottle mt-20 mini__card x' id='abs' src={bottle1} alt="bottle1" />
             }
-            {count == 2 && <label className="select__card mini__card two in">
-                <img id='fx-2' src={two} />
-                <div className='mb-15 mt-40 txt-al'>
-                    <h3 className='mb-5'>مـدة شهر </h3>
-                    <h5 className='clr'>عبـوتين</h5>
-                    <h5 className='price mt-10'><span className='pr'>بــ</span><span className='gr'> 800 </span> ريـال سـعودي</h5>
-                </div>
-            </label>
+            {count == 2 && <img className='bottle2 mt-20 mini__card y' id='abs' src={bottle2} alt="bottle2" />
             }
-            {count == 4 && <label className="select__card mini__card two in" >
-                <img id='fx-4' src={four} />
-                <div className='mb-15 mt-40 txt-al'>
-                    <h3 className='mb-5'>مـدة شـهرين </h3>
-                    <h5 className='clr'>عبـوات 4</h5>
-                    <h5 className='price mt-10'><span className='pr'>بــ</span><span className='gr'> 1600 </span> ريـال سـعودي</h5>
-                </div>
-            </label>
+            {count == 4 && <img className='bottle4 mt-20 mini__card y' id='abs' src={bottle4} alt="bottle4" />
             }
-            {count == 6 && <label className="select__card mini__card two in" >
-                <img id='fx-6' src={six} />
-                <div className='mb-15 mt-40 txt-al'>
-                    <h3 className='mb-5'>مــدة<br /> ثـلاث شهور </h3>
-                    <h5 className='clr'>عبـوات 6</h5>
-                    <h5 className='price mt-10'><span className='pr'>بــ</span><span className='gr'> 2400 </span> ريـال سـعودي</h5>
-                </div>
-            </label >
+            {count == 6 && <img className='bottle6 mt-20 mini__card y' id='abs' src={bottle6} alt="bottle6" />
             }
-            {count == 8 && <label className="select__card mini__card two in" >
-                <img id='fx-8' src={eight} />
-                <div className='mb-15 mt-40 txt-al'>
-                    <h3 className='mb-5'>مـدة شهرين </h3>
-                    <h5 className='clr'>عبوات 8</h5>
-                    <h5 className='price mt-10'><span className='pr'>بــ</span><span className='gr'> 3200 </span> ريـال سـعودي</h5>
-                </div>
-            </label>
+            {count == 8 && <img className='bottle8 mt-20 mini__card y' id='abs' src={bottle8} alt="bottle8" />
             }
-            {count == "Compo 1" && <label className="select__card mini__card three six in">
-                <img id='fx-6-s' src={syringe1} />
-                <img id='fx-6-a' src={six} />
-                <div className='mb-180 mt-10 txt-al'>
-                    <h3 className='mb-5'>مـدة<br /> ثـلاث شهور </h3>
-                    <h6 className='mb-5 ml-60 clr'>{age === 'less' ? 'عبـوات 6' : 'عبـوة 12'}</h6>
-                    <h6 className='ml-75 clr'> + {age === 'less' ? 'جلسـتين' : 'جلسـات 4'}<br /> وخــز تحفـيزي</h6>
-                    <h5 className='price mt-15'><span className='pr'>بــ</span><span className='gr'> {age === 'less' ? '3300' : '5800'} </span> ريـال سـعودي</h5>
-                </div>
-            </label>
+            {count === "Compo 1" && age === 'less' && <img className='bottle6x2 mt-20 mini__card z' id='abs' src={bottle6x2} alt="bottle6x2" />
             }
-            {count == "Compo 2" && <label className="select__card mini__card three in" >
-                <img id='fx-12-4' src={syringe2} />
-                <img id='fx-12-1' src={four} />
-                <img id='fx-12-2' src={eight} />
-                {age === 'less' && <img id='fx-12-2' className='on-hv' src={eight} />}
-                <div className='mb-180 mt-25 txt-al'>
-                    <h3 className='mb-5'>مـدة<br /> ستـة شهور </h3>
-                    <h6 className='mb-5 ml-60 clr'>عبـوة {age === 'less' ? '12' : '24'}</h6>
-                    <h6 className='ml-75 clr'> + جلسـات 4<br /> وخــز تحفـيزي</h6>
-                    <h5 className='price mt-20'><span className='pr'>بــ</span><span className='gr'> {age === 'less' ? '6500' : '11500'} </span> ريـال سـعودي</h5>
-                </div>
-            </label>
+            {count === "Compo 2" && age === 'less' && <img className='bottle12x4 mt-20 mini__card z' id='abs' src={bottle12x4} alt="bottle12x4" />
             }
-
+            {count === "Compo 1" && age === 'greater' && < img className='bottle12x2 mt-20 mini__card z' id='abs' src={bottle12x2} alt="bottle12x2" />
+            }
+            {count === "Compo 2" && age === 'greater' && <img className='bottle24x4 mt-20 mini__card z' id='abs' src={bottle24x4} alt="bottle24x4" />
+            }
         </>
     )
 }
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,8 +3,11 @@ import { useLocation } from 'react-router-dom';
 import Massage from '../components/Massage';
 import main from '/public/main-video.mp4';
 import { Link } from "react-router-dom";
+import logo from '/public/favicon.png';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
 
-// import sec from '//public/sec-main video.mp4';
+// import sec from '/public/sec-main video.mp4';
 // import thred from '/public/sec-video.mp4';
 
 function Checkout() {
@@ -40,36 +43,38 @@ function Checkout() {
 
     return (
 
-
-        <div className="bside">
-            <div className="c-s">
-                <Massage count={units} age={age} />
+        <>
+            <div className="bside">
+                <div className="c-s">
+                    <Massage count={units} age={age} />
+                </div>
+                <div className="t-p">
+                    <h3>تفصيل التكلفة:</h3>
+                    {age === 'less' && <p ><h4>إجمالي تكلفة المنتج:</h4> <span id='p-clr'> {units === "Compo 1" ? '3300' : units === "Compo 2" ? '6500' : units == "" ? "--" : costDetails.productCost}</span>  ريال</p>}
+                    {age === 'greater' && <p><h4>إجمالي تكلفة المنتج:</h4> <span id="p-clr">{units === "Compo 1" ? '5800' : units === "Compo 2" ? '11500' : units == "" ? "--" : costDetails.productCost}</span>  ريال</p>}
+                    <>
+                        <p><h4>إجمالي تكلفة الشحن:</h4> <span id="p-clr">{units === "Compo 1" || units === "Compo 2" || units == "" ? '--' : costDetails.shipmentCost}</span> ريال</p>
+                        <p><h4>قيمة الضريبة :</h4> <span id="p-clr">{units === "Compo 1" || units === "Compo 2" || units == "" ? '--' : costDetails.tax}</span> ريال</p>
+                        <p><h4>التكلفة الإجمالية:</h4> <span id="p-clr">{units === "Compo 1" || units === "Compo 2" || units == "" ? '--' : costDetails.totalCost}</span> ريال</p></>
+                </div>
+                {units !== "" ? <a className='submit__btn ad' href="https://store.nndprochildren.com/%D8%A7%D9%84%D9%85%D9%83%D9%85%D9%84-%D8%A7%D9%84%D8%BA%D8%B0%D8%A7%D8%A6%D9%8A-%D8%A7%D9%84%D8%B7%D8%A8%D9%8A%D8%B9%D9%8A-%D8%A7%D9%86-%D8%A7%D9%86-%D8%AF%D9%8A-%D9%84%D9%84%D8%A3%D8%B7%D9%81%D8%A7%D9%84/p496109765">تابع الشراء</a> :
+                    <Link className='submit__btn ad' to="/Options">الرجوع الي قائمة العروض</Link>
+                }
             </div>
-            <div className="t-p">
-                <h3>تفصيل التكلفة:</h3>
-                {age === 'less' && <p ><h5>إجمالي تكلفة المنتج:</h5> <span id='p-clr'> {units === "Compo 1" ? '3300' : units === "Compo 2" ? '6500' : units == "" ? "--" : costDetails.productCost}</span>  ريال</p>}
-                {age === 'greater' && <p><h5>إجمالي تكلفة المنتج:</h5> <span id="p-clr">{units === "Compo 1" ? '5800' : units === "Compo 2" ? '11500' : units == "" ? "--" : costDetails.productCost}</span>  ريال</p>}
-                <>
-                    <p><h5>إجمالي تكلفة الشحن:</h5> <span id="p-clr">{units === "Compo 1" || units === "Compo 2" || units == "" ? '--' : costDetails.shipmentCost}</span> ريال</p>
-                    <p><h5>قيمة الضريبة :</h5> <span id="p-clr">{units === "Compo 1" || units === "Compo 2" || units == "" ? '--' : costDetails.tax}</span> ريال</p>
-                    <p><h5>التكلفة الإجمالية:</h5> <span id="p-clr">{units === "Compo 1" || units === "Compo 2" || units == "" ? '--' : costDetails.totalCost}</span> ريال</p></>
-            </div>
-            {units !== "" ? <a className='submit__btn ad' href="https://store.nndprochildren.com/%D8%A7%D9%84%D9%85%D9%83%D9%85%D9%84-%D8%A7%D9%84%D8%BA%D8%B0%D8%A7%D8%A6%D9%8A-%D8%A7%D9%84%D8%B7%D8%A8%D9%8A%D8%B9%D9%8A-%D8%A7%D9%86-%D8%A7%D9%86-%D8%AF%D9%8A-%D9%84%D9%84%D8%A3%D8%B7%D9%81%D8%A7%D9%84/p496109765">أكمـل عمليـة الشراء</a> :
-                <Link className='submit__btn ad' to="/Options">الرجوع الي قائمة العروض</Link>
-            }
-        </div>
-
+        </>
     );
 }
-
 
 function FixedSide() {
     return (
         <>
             <div className="aside">
-                <video className='m-v
-                    ' src={main} type="video/mp4" controls autoPlay muted>
-                </video>
+                <div>
+                    <video className='m-v
+                    ' src={main} type="video/mp4" controls muted>
+                    </video>
+                </div>
+
             </div>
         </>
     )
@@ -77,12 +82,59 @@ function FixedSide() {
 
 
 export default function Details() {
+
+    const logoRef = useRef(null);
+
+
+    useEffect(() => {
+        gsap.fromTo(
+            logoRef.current,
+            { y: 1000, opacity: 0 }, // Start from above with opacity 0
+            { duration: 2, y: 0, opacity: 0.2, ease: 'power.out' } // End with original position and opacity
+        );
+
+    }, []);
+
+
     return (
         <>
             <div className='checkout__page container'>
-                <FixedSide />
-                <Checkout />
+                <div className="big__screens">
+                    <Checkout />
+                </div>
+                <section className='main__art'>
+                    <div className="screen__moblie">
+                        <Checkout />
+                    </div>
+                    <div className="col__section">
+                        <article className='info___col___section '>
+                            <div className="t-art">
+                                <div className="check__logo">
+                                    <img ref={logoRef} src={logo} alt="logo" />
+
+                                    <h1>NND Pro Childern</h1>
+
+                                    <p>
+                                        <strong style={{ color: '#ff7d25' }}>مكمل غذائي</strong> فريد يضم
+                                        <strong style={{ color: '#00c0c7' }}> فيتامينات</strong>،
+                                        <strong style={{ color: '#ff9495' }}> معادن</strong>،
+                                        <strong style={{ color: '#ff7d25' }}> خضروات</strong>،
+                                        <strong style={{ color: '#00c0c7' }}> دهون</strong>، و
+                                        <strong style={{ color: '#ff9495' }}> أحماض أمينية</strong> من مصادر طبيعية تم تصميمه واختيار مكوناته بعناية وفقًا لمعايير
+                                        <strong style={{ color: '#ff7d25' }}> العلوم العصبية</strong> وتمت مراجعته وتحليله لضمان
+                                        <strong style={{ color: '#00c0c7' }}> الجودة</strong> و
+                                        <strong style={{ color: '#ff9495' }}> الفعالية</strong>.
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                        <FixedSide />
+                    </div>
+                </section>
             </div>
         </>
     )
 }
+
+
+
